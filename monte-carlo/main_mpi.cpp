9132e1c077c0af4369ec7f86b61233c70d34b489
PawelD16 <pawel200216@interia.eu>
2025-05-27 19:46:58 +0200

    chore: refactor

--- monte-carlo/main_mpi.cpp
+++ monte-carlo/main_mpi.cpp
@@ -5,42 +5,42 @@
 
 #include "functions.h"
 
-int main(int argc, char *argv[]) 
+const int PARENT_PROCESS_ID = 0;
+
+int main(int argc, char *argv[])
 {
     MPI_Init(&argc, &argv); // initialize MPI environment
 
     // MPI_COMM_WORLD - communicator (contains all the processes)
     int rank, size;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);  // current process ID
-    MPI_Comm_size(MPI_COMM_WORLD, &size);  // total number of processes
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank); // current process ID
+    MPI_Comm_size(MPI_COMM_WORLD, &size); // total number of processes
 
     try
     {
-        auto [a, b, samples] = readParams(argc, argv);
+        auto [lower_bound, upper_bound, samples] = readArgs(argc, argv);
 
         long long sample_size = samples / size;
-        double local = monteCarlo(a, b, sample_size);
+        double local = monteCarlo(lower_bound, upper_bound, sample_size);
         double total = 0.0;
 
-        // perform reduction with a sum operation (MPI_SUM), 
+        // perform reduction with a sum operation (MPI_SUM),
         // the type is double
         // 1 is the number of elements in the send buffer (1st param)
         // 0 is the rank of the root process
-        MPI_Reduce(&local, &total, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+        MPI_Reduce(&local, &total, 1, MPI_DOUBLE, MPI_SUM, PARENT_PROCESS_ID, MPI_COMM_WORLD);
 
-        if (rank == 0) 
+        if (rank == PARENT_PROCESS_ID)
         {
-            total /= size;  // Average result from all processes
+            total /= size; // Average result from all processes
             std::cout << "Estimated integral: " << total << std::endl;
         }
-
     }
     catch (...)
     {
         MPI_Finalize();
         return 1;
     }
- 
 
     MPI_Finalize();
     return 0;
